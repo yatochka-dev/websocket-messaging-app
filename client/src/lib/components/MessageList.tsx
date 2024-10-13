b8665761271a828bc5c2fff4b4968007f61a1077
Yatochka <philipchef13@gmail.com>
2024-10-13 20:08:44 +0300

    Change for the sake of change

--- client/src/lib/components/MessageList.tsx
+++ client/src/lib/components/MessageList.tsx
@@ -11,7 +11,7 @@ interface MessageListProps {
 
 const MessageList: React.FC<MessageListProps> = ({ messages }) => {
     return (
-        <ul className={"flex flex-col gap-4 bg-gray-200 shadow-md p-4 rounded-lg"}>
+        <ul className={"flex flex-col gap-4 bg-gray-200 shadow-md p-4 rounded-lg "}>
             {messages.map((msg, index) => (
                 <li key={index}>
                     <strong>{msg.name}:</strong> {msg.message}
